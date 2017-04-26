53c05b930347dfd6db3223ac163a9451f7355b60
Matt Guo <matt@mattguo.com>
2017-04-25 17:58:30 -0700

    assign user/pwd to zkweb

--- zkweb.sh
+++ zkweb.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 8080:8080 --link myzk:myzk -e ZK_DEFAULT_NODE=myzk:2181 --name zk-web -t tobilg/zookeeper-webui
+docker run --rm -p 8080:8080 --link myzk:myzk -e USER=mguo -e PASSWORD=mono -e ZK_DEFAULT_NODE=myzk:2181 --name zk-web -t tobilg/zookeeper-webui
